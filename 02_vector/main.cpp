f06cb24f804bd0dc67e1e89150c605f720cf9404
takumi.yoshida <takumi.yoshida.2265@gmail.com>
2024-12-12 19:35:18 +0900

    Modify 02_vector

--- 02_vector/main.cpp
+++ 02_vector/main.cpp
@@ -22,26 +22,3 @@ void free_vector_data(const int * data)
 {
     delete[] data;
 }
-
-
-// int MAX = 101;
-
-// extern "C"
-// int * get_vector(int & n, int seed)
-// {
-//     // std::mt19937 rng(seed);
-//     // n = std::uniform_int_distribution<>(1, MAX)(rng);
-//     std::cout << "n=" << n << std::endl;
-
-//     // auto * vec = new std::vector<int>(n);
-//     // for (int i = 0; i < n; ++i)
-//     //     (*vec)[i] = std::uniform_int_distribution<>(1, MAX)(rng);
-//     // return vec->data();
-//     return &n;
-// }
-
-// extern "C"
-// void free_vector(int * vec)
-// {
-//     delete[] vec;
-// }
